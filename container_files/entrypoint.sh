19fe034dad9b5d5b631f1a9299490cbd34b2ec48
Raphael Mader <mader.raphael@gmx.at>
2022-06-23 18:15:28 +0200

    Start session on startup

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 mkdir -p /tmp/lib/openvpn3/configs/
-exec /usr/bin/dbus-daemon --nofork --nopidfile --system
+/usr/bin/dbus-daemon --fork --nopidfile --system
+openvpn3 session-start --dco true --config /netherlands.ovpn
+exec su openvpn sleep infinity
