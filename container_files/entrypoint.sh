ad476d31e3ef3a48c828fee02d6b7204e4739ce6
Raphael Mader <mader.raphael@gmx.at>
2022-06-23 18:46:50 +0200

    Create directory in Dockerfile

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 addgroup -g $PGID openvpn
-adduser -S -D openvpn -u $PUID-G openvpn
+adduser -S -D openvpn -u $PUID -G openvpn
+
+mkdir -p /tmp/lib/openvpn3/configs/
 
 (sleep 10; openvpn3 session-start --dco true --config /netherlands.ovpn) &
 exec /usr/bin/dbus-daemon --nofork --nopidfile --system
