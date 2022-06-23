6a265d5fdecc222b620b5e82a49f5f411ef96fbf
Raphael Mader <mader.raphael@gmx.at>
2022-06-24 00:17:00 +0200

    Allow all loopback communication

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -17,12 +17,11 @@ ip46tables -A OUTPUT -o $VPN_IF -j ACCEPT
 # Allow only VPN traffic (udp port 1194) on the actual interface.
 ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 1194 -j ACCEPT
 
-# Allow DNS (tcp/udp port 53) on all interfaces (docker provides a local DNS on the loopback interface).
-ip46tables -A OUTPUT -p udp --dport 53 -j ACCEPT
-ip46tables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+# Allow loopback communication.
+ip46tables -A OUTPUT -o lo -j ACCEPT
 
 # Allow established/related traffic.
 ip46tables -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-(su openvpn; sleep 10; openvpn3 session-start --dco ${ENABLE_DCO:-false} --config $VPN_CONFIG &)
+(su openvpn && sleep 10 && openvpn3 session-start --dco ${ENABLE_DCO:-false} --config $VPN_CONFIG &)
 exec /usr/bin/dbus-daemon --nofork --nopidfile --system
