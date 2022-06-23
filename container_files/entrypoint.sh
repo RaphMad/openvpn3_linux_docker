07ec07bb3dbbe31881910e34ea09fd341438b04f
Raphael Mader <mader.raphael@gmx.at>
2022-06-23 18:25:05 +0200

    Use runuser

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 mkdir -p /tmp/lib/openvpn3/configs/
+
 /usr/bin/dbus-daemon --fork --nopidfile --system
 openvpn3 session-start --dco true --config /netherlands.ovpn
-exec su openvpn 'sleep infinity'
+
+exec runuser -u openvpn sleep infinity
