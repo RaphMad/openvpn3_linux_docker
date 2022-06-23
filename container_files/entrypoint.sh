c4ce734951f30dba487342a94a481f19ebcaed5f
Raphael Mader <mader.raphael@gmx.at>
2022-06-23 23:52:23 +0200

    Refine firewall rules

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-iptables -P OUTPUT DROP
-iptables -A OUTPUT -o tun0 -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp --match multiport --dports 1194,53 -j ACCEPT
+ip46tables () {
+    iptables $@
+    ip6tables $@
+}
 
-ip6tables -P OUTPUT DROP
-ip6tables -A OUTPUT -o tun0 -j ACCEPT
-ip6tables -A OUTPUT -o eth0 -p udp --match multiport --dports 1194,53 -j ACCEPT
+# Disable all outgoing traffic that is not explicitely allowed.
+ip46tables -P OUTPUT DROP
+
+# Allow all outgoing traffic for the VPN tunnel interface.
+ip46tables -A OUTPUT -o tun0 -j ACCEPT
+
+# Allow only VPN traffuc (udp port 1194) on the actual interface.
+ip46tables -A OUTPUT -o eth0 -p udp --dport 1194 -j ACCEPT
+
+# Allow DNS (udp port 53) on all interfaces.
+ip46tables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
 (su openvpn; sleep 10; openvpn3 session-start --dco ${ENABLE_DCO:-false} --config $VPN_CONFIG &)
 exec /usr/bin/dbus-daemon --nofork --nopidfile --system
