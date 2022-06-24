e07a183940afbd68f89499c3929a1ce84a09a8d7
Raphael Mader <mader.raphael@gmx.at>
2022-06-24 12:18:11 +0200

    Reorder firewall rules

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -5,34 +5,35 @@ ip46tables() {
     ip6tables $@
 }
 
-to_hex() {
+to_dns_hex() {
     IFS=.
     for i in ${1}; do
+        # See https://stackoverflow.com/a/17184231/3324111
         echo -n "|${#i}|$i"
     done
 }
 
-DOCKER_IF=eth0
 VPN_IF=tun0
+DOCKER_IF=eth0
 
 # Disable all outgoing traffic that is not explicitely allowed.
 ip46tables -P OUTPUT DROP
 
-# Allow all outgoing traffic for the VPN tunnel interface.
+# Allow all outgoing traffic for the VPN interface.
 ip46tables -A OUTPUT -o $VPN_IF -j ACCEPT
-
-# Allow only VPN traffic (udp port 1194) on the actual interface.
-ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 1194 -j ACCEPT
-
-# Allow DNS (tcp/udp port 53) on the docker interface for looking up VPN endpoints DNS name.
-ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 53 -m string --hex-string $(to_hex $VPN_HOST) --algo bm -j ACCEPT
-ip46tables -A OUTPUT -o $DOCKER_IF -p tcp --dport 53 -m string --hex-string $(to_hex $VPN_HOST) --algo bm -j ACCEPT
 
 # Allow loopback communication.
 ip46tables -A OUTPUT -o lo -j ACCEPT
 
+# Allow only VPN traffic (udp port 1194) on the docker interface.
+ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 1194 -j ACCEPT
+
 # Allow established/related traffic.
 ip46tables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow DNS (tcp/udp port 53) on the docker interface only for looking up the VPN endpoints DNS name.
+ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 53 -m string --hex-string $(to_dns_hex $VPN_HOST) --algo bm -j ACCEPT
+ip46tables -A OUTPUT -o $DOCKER_IF -p tcp --dport 53 -m string --hex-string $(to_dns_hex $VPN_HOST) --algo bm -j ACCEPT
 
 (su openvpn && sleep 10 && openvpn3 session-start --dco ${ENABLE_DCO:-false} --config $VPN_CONFIG &)
 exec /usr/bin/dbus-daemon --nofork --nopidfile --system
