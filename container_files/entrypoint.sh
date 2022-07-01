e2257937305da43c3678c6e83a34dc903000f119
Raphael Mader <mader.raphael@gmx.at>
2022-07-01 16:39:04 +0200

    Add sensible defaults for env vars

--- container_files/entrypoint.sh
+++ container_files/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+DOCKER_IF=${DOCKER_IF:-eth0}
+ENABLE_DCO=${ENABLE_DCO:-false}
+VPN_IF=${VPN_IF:-tun0}
+VPN_PROTO=${VPN_PROTO:-udp}
+VPN_PORT=${VPN_PORT:-1194}
+
 ip46tables() {
     iptables $@
     ip6tables $@
@@ -14,9 +20,6 @@ to_dns_hex() {
     unset IFS
 }
 
-VPN_IF=tun0
-DOCKER_IF=eth0
-
 # Disable all outgoing traffic that is not explicitely allowed.
 ip46tables -P OUTPUT DROP
 
@@ -26,15 +29,31 @@ ip46tables -A OUTPUT -o $VPN_IF -j ACCEPT
 # Allow loopback communication.
 ip46tables -A OUTPUT -o lo -j ACCEPT
 
-# Allow only VPN traffic (udp port 1194) on the docker interface.
-ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 1194 -j ACCEPT
+# Allow only VPN traffic on the docker interface.
+ip46tables -A OUTPUT -o $DOCKER_IF -p $VPN_PROTO --dport $VPN_PORT -j ACCEPT
 
 # Allow established/related traffic.
 ip46tables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # Allow DNS (tcp/udp port 53) on the docker interface only for looking up the VPN endpoints DNS name.
-ip46tables -A OUTPUT -o $DOCKER_IF -p udp --dport 53 -m string --hex-string $(to_dns_hex $VPN_HOST) --algo bm -j ACCEPT
-ip46tables -A OUTPUT -o $DOCKER_IF -p tcp --dport 53 -m string --hex-string $(to_dns_hex $VPN_HOST) --algo bm -j ACCEPT
+if [ -n $VPN_HOST ]; then
+    ip46tables -A OUTPUT \
+               -o $DOCKER_IF \
+               -p udp \
+               --dport 53 \
+               -m string \
+               --hex-string $(to_dns_hex $VPN_HOST) \
+               --algo bm \
+               -j ACCEPT
 
-(su openvpn && sleep 10 && openvpn3 session-start --dco ${ENABLE_DCO:-false} --config $VPN_CONFIG &)
+    ip46tables -A OUTPUT \
+               -o $DOCKER_IF \
+               -p tcp \
+               --dport 53 \
+               -m string \
+               --hex-string $(to_dns_hex $VPN_HOST) \
+               --algo bm -j ACCEPT
+fi
+
+(su openvpn && sleep 10 && openvpn3 session-start --dco $ENABLE_DCO --config $VPN_CONFIG &)
 exec /usr/bin/dbus-daemon --nofork --nopidfile --system
